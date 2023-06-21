9059369cdfc70de526cdb5344a98a8e955154c5d
hadami <hadami16@naver.com>
2023-06-21 21:32:29 +0900

    Initial Commit

---
+++ WeatherList/WeatherListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherListApp.swift
+//  WeatherList
+//
+//  Created by chloe on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
